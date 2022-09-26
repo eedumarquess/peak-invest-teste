0a883b572fff705d6668beb93ac2b5eb82c340c6
Edu <eedumarquess@gmail.com>
2022-09-25 21:29:53 -0300

    Create Program.cs with routes

---
+++ back-peak/back-peak/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using static back_peak.Controllers.CalculusController;
+using static back_peak.Controllers.RegisterController;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/{id}", (int id) =>
+{
+    var result = Register(id);
+
+    return result;
+});
+
+app.MapPost("/value={value}&parcel={parcel}", (float value, int parcel) =>
+{
+    var result = Calculus(value, parcel);
+
+    return result;
+});
+
+app.Run();
