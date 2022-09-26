a22f85ba1d92fb86f5c5af13452cb58a99f54689
Edu <eedumarquess@gmail.com>
2022-09-25 21:29:08 -0300

    Create controllers for calculus/register

---
+++ back-peak/back-peak/Controllers/calculusController.cs
@@ -0,0 +1,16 @@
+﻿namespace back_peak.Controllers
+
+{
+	public static class CalculusController
+	{
+		public static double Calculus(float value, int parcel)
+		{
+            var totalValue = value * parcel;
+
+            var finalValue = totalValue + (0.05 * totalValue);
+
+            return finalValue;
+        }
+	}
+}
+
---
+++ back-peak/back-peak/Controllers/registerController.cs
@@ -0,0 +1,20 @@
+﻿
+namespace back_peak.Controllers
+
+{
+	public static class RegisterController
+	{
+		public static string Register(int id)
+		{
+            var list = new List<KeyValuePair<int, string>>
+            {
+                new KeyValuePair<int, string>(1, "Joao"),
+                new KeyValuePair<int, string>(2, "Maria"),
+                new KeyValuePair<int, string>(3, "Ana")
+            };
+
+            return list.First(c => c.Key == id).Value;
+		}
+	}
+}
+
